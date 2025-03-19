1d307943d75d30a44153cb8afa446ddbca8b9be0
ummar <ummar.asyraf7@gmail.com>
2025-03-19 21:29:47 +0800

    Configured for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import {preprocessMeltUI, sequence} from "@melt-ui/pp";
+import { preprocessMeltUI, sequence } from "@melt-ui/pp";
 import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config}*/
+
+const dev = process.argv.includes('dev');
+const repoName = 'AlgeMaster'; // Change this to your GitHub repo name
+
 const config = {
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter()
+    adapter: adapter(),
+    paths: {
+      base: dev ? '' : `/${repoName}`
+    },
+    appDir: 'internal'
   },
   preprocess: sequence([preprocessMeltUI()])
 };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'AlgeMaster';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), tailwindcss()],
+	base: `/${repoName}/`
+
 });
