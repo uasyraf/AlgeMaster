c0412561fbaf2f3aa902c2aeff73929e2e34ce1c
ummar <ummar.asyraf7@gmail.com>
2025-03-19 21:35:55 +0800

    update package

--- svelte.config.js
+++ svelte.config.js
@@ -10,11 +10,7 @@ const config = {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter({
-      pages: 'build',  // Ensures SvelteKit outputs to the correct folder
-      assets: 'build',
-      fallback: 'index.html' // Ensures correct routing on GitHub Pages
-    }),
+    adapter: adapter(),
     paths: {
       base: dev ? '' : `/${repoName}`
     },
