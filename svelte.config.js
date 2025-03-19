b5652d1ead65c095933d4d503dd496bdccbb8080
ummar <ummar.asyraf7@gmail.com>
2025-03-19 23:12:22 +0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
     }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-      assets: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+      assets: process.argv.includes('dev') ? '' : 'https://uasyraf.github.io' + process.env.BASE_PATH,
     }
   }
 };
