b948dcbec7aa43dbcd693c38afd6415220b81580
ummar <ummar.asyraf7@gmail.com>
2025-03-19 22:07:05 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = 'AlgeMaster';
-
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
-	base: `/${repoName}/`
-
 });
